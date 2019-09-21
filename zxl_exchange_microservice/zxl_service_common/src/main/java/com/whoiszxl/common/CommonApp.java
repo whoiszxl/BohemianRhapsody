a3e4a5a600aa98c16bfb5b933432d44a26398efd
zhouxiaolong <whoiszxl@gmail.com>
2019-09-21 10:18:28 +0800

    mvn 配置优化

--- zxl_exchange_microservice/zxl_service_common/src/main/java/com/whoiszxl/common/CommonApp.java
+++ zxl_exchange_microservice/zxl_service_common/src/main/java/com/whoiszxl/common/CommonApp.java
@@ -3,14 +3,19 @@ package com.whoiszxl.common;
 import com.whoiszxl.base.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages={"com.whoiszxl.base"})//扫描base包下的类
 @ComponentScan(basePackages={"com.whoiszxl.common"})//扫描本项目下的所有类
+@EnableJpaRepositories(basePackages = {"com.whoiszxl.common.dao"})
+@ComponentScan(basePackages={"com.whoiszxl.base"})//扫描base包和wallet base下的类
+@EntityScan(basePackages = {"com.whoiszxl.common.pojo"})
 public class CommonApp {
 
     public static void main(String[] args) {
