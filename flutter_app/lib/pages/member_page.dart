28b045d8c41dbe89a8ea72bac8b4193d8666fbc8
zhouxiaolong <whoiszxl@gmail.com>
2019-10-05 15:04:10 +0800

    flutter 资产控件添加

--- flutter_app/lib/pages/member_page.dart
+++ flutter_app/lib/pages/member_page.dart
@@ -11,7 +11,8 @@ class MemberPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          _topHeader()
+          _topHeader(),
+          _myAsset()
         ],
       ),
     );
@@ -32,7 +33,7 @@ class MemberPage extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Container(
-            height: ScreenUtil().setHeight(200),
+            height: ScreenUtil().setHeight(160),
             
             margin: EdgeInsets.only(top: 30),
             child: ClipOval(
@@ -48,6 +49,24 @@ class MemberPage extends StatelessWidget {
     );
   }
 
+
+  Widget _myAsset() {
+    return Container(
+      margin: EdgeInsets.only(top: 3),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1, color: Colors.black12)
+        )
+      ),
+      child: ListTile(
+        title: Text('币币总资产折合(BTC)', style: TextStyle(fontSize: ScreenUtil().setSp(20), color: Colors.black54)),
+        subtitle: Text('0.1345775≈16778.54 CNY', style: TextStyle(fontSize: ScreenUtil().setSp(30), color: Colors.black87)),
+        trailing: Icon(Icons.account_balance_wallet),
+
+      ),
+    );
+  }
 
 }
 
