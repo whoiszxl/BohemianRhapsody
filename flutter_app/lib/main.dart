bc4197f39524e1907f66dcb8d68e43cf78a66889
zhouxiaolong <whoiszxl@gmail.com>
2019-10-09 19:01:11 +0800

    flutter 一些优化

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/utils/SpUtils.dart';
 import './pages/index_page.dart';
-import './routers/routers.dart';
-import './routers/application.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,12 +8,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    //-------------------主要代码start
-    final router = Router();
-    Routes.configureRoutes(router);
-    Application.router=router;
-    //-------------------主要代码end
 
     //初始化spUtils
     SpUtils.getInstance();
--- flutter_app/lib/utils/SpUtils.dart
+++ flutter_app/lib/utils/SpUtils.dart
@@ -5,8 +5,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SpUtils {
   static SharedPreferences preferences;
   static Future<bool> getInstance() async{
-     preferences = await SharedPreferences.getInstance();
-     return true;
+    preferences = await SharedPreferences.getInstance();
+    return true;
   }
 
   static void addString(String key, String value) {
@@ -19,6 +19,21 @@ class SpUtils {
 
   static void clearString(String key) {
     preferences.remove(key);
+  }
+
+  static void addStringAsync(String key, String value) async {
+    var sp = await SharedPreferences.getInstance();
+    sp.setString(key, value);
+  }
+
+  static Future getStringAsync(String key) async {
+    var sp = await SharedPreferences.getInstance();
+    return sp.getString(key);
+  }
+
+  static void clearStringAsync(String key) async {
+    var sp = await SharedPreferences.getInstance();
+    sp.remove(key);
   }
 }
 
