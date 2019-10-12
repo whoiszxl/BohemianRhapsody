e497ce8d422952fab05882afdc000deae171ad78
zhouxiaolong <whoiszxl@gmail.com>
2019-10-12 15:23:06 +0800

    java common 模块跨域

--- zxl_exchange_microservice/zxl_service_common/src/main/java/com/whoiszxl/common/CommonApp.java
+++ zxl_exchange_microservice/zxl_service_common/src/main/java/com/whoiszxl/common/CommonApp.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -25,5 +28,25 @@ public class CommonApp {
     @Bean
     public IdWorker idWorker() {
         return new IdWorker(1, 1);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  //可访问ip，ip最好从配置文件中获取，
+                        .allowedMethods("PUT", "DELETE","GET","POST")
+                        .allowedHeaders("*")
+                        .exposedHeaders("access-control-allow-headers",
+                                "access-control-allow-methods",
+                                "access-control-allow-origin",
+                                "access-control-max-age",
+                                "X-Frame-Options")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+
     }
 }
