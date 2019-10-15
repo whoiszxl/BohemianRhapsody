b7938921e84d916c0f6298783f651dfdb9b41bd0
zhouxiaolong <whoiszxl@gmail.com>
2019-10-15 17:42:57 +0800

    flutter 充值地址二维码生成

--- flutter_app/lib/config/app_const.dart
+++ flutter_app/lib/config/app_const.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppConst {
+  static int successCode = 0;
   static Color themeMainColor = Colors.blue; 
 }
 
--- flutter_app/lib/pages/member/recharge_detail_page.dart
+++ flutter_app/lib/pages/member/recharge_detail_page.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_app/config/app_const.dart';
 import 'package:flutter_app/service/service_method.dart';
+import 'package:flutter_app/utils/ToastUtils.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class RechargeDetailPage extends StatefulWidget {
   final String currencyId;
@@ -36,7 +39,23 @@ class _RechargeDetailPageState extends State<RechargeDetailPage> {
           if(snapshot.hasData) {
             print("获取到的用户地址" + snapshot.data.toString());
             var addressData = json.decode(snapshot.data);
-            return Text(addressData['data']['rechargeAddress'].toString());
+            if(addressData['code'] != AppConst.successCode) {
+              ToastUtils.showMessage(addressData['message']);
+            }
+            return QrImage(
+              data: addressData['data']['rechargeAddress'],
+              version: QrVersions.auto,
+              size: 180,
+              gapless: false,
+              errorStateBuilder: (context, err) {
+                return Container(
+                  child: Text(
+                    "Uh oh! Something went wrong...",
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              },
+            );
           }else {
             return Container(
               alignment: Alignment.center,
