08c4a137e0c6558a45eb19d372f0d651064900a0
zhouxiaolong <whoiszxl@gmail.com>
2019-10-15 19:31:50 +0800

    flutter 充值地址界面的布局与参数填充

--- flutter_app/lib/config/app_const.dart
+++ flutter_app/lib/config/app_const.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppConst {
   static int successCode = 0;
-  static Color themeMainColor = Colors.blue; 
+  static Color themeMainColor = Colors.blue;
+  static Color themeGreyColor = Color.fromRGBO(235, 235, 235, 1);
 }
 
--- flutter_app/lib/pages/member/recharge_detail_page.dart
+++ flutter_app/lib/pages/member/recharge_detail_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/config/app_const.dart';
 import 'package:flutter_app/service/service_method.dart';
 import 'package:flutter_app/utils/ToastUtils.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class RechargeDetailPage extends StatefulWidget {
@@ -42,19 +43,12 @@ class _RechargeDetailPageState extends State<RechargeDetailPage> {
             if(addressData['code'] != AppConst.successCode) {
               ToastUtils.showMessage(addressData['message']);
             }
-            return QrImage(
-              data: addressData['data']['rechargeAddress'],
-              version: QrVersions.auto,
-              size: 180,
-              gapless: false,
-              errorStateBuilder: (context, err) {
-                return Container(
-                  child: Text(
-                    "Uh oh! Something went wrong...",
-                    textAlign: TextAlign.center,
-                  ),
-                );
-              },
+            String rechargeAddress = addressData['data']['rechargeAddress'];
+            return Column(
+              children: <Widget>[
+                addressHeader(currencyId: currencyId),
+                addressDetail(rechargeAddress: rechargeAddress)
+              ],
             );
           }else {
             return Container(
@@ -66,5 +60,94 @@ class _RechargeDetailPageState extends State<RechargeDetailPage> {
       ),
     );
   }
+}
 
+
+
+
+class addressHeader extends StatelessWidget {
+
+  String currencyId;
+
+  addressHeader({Key key, this.currencyId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 10),
+          alignment: Alignment.centerLeft,
+          child: Text('充币', textAlign: TextAlign.left, style: TextStyle(fontSize: ScreenUtil().setSp(64), fontWeight: FontWeight.bold))),
+        Container(
+          margin: EdgeInsets.only(right: 10, left: 10, top: 15),
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          color: AppConst.themeGreyColor,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(child: Text('BTC', style: TextStyle(fontWeight: FontWeight.bold),), margin: EdgeInsets.only(left: 8),),
+              Container(child: Text('选择币种 >'), margin: EdgeInsets.only(right: 8)),
+            ],
+          ),
+        ),
+
+      ],
+    );
+  }
+}
+
+
+class addressDetail extends StatelessWidget {
+
+  String rechargeAddress;
+
+  addressDetail({Key key, this.rechargeAddress}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 10, left: 10, top: 10),
+      padding: EdgeInsets.only(top: 25, bottom: 15),
+      width: ScreenUtil().setWidth(750),
+      color: AppConst.themeGreyColor,
+      child: Column(children: <Widget>[
+        _qrcodeWidget(rechargeAddress),
+        Container(child: OutlineButton(
+          borderSide: new BorderSide(color: AppConst.themeMainColor),
+          child: new Text('保存二维码', style: TextStyle(color: AppConst.themeMainColor)),
+          onPressed: () {
+            
+          },
+        ),),
+        Text('充币地址', textAlign: TextAlign.center),
+        Text(rechargeAddress, textAlign: TextAlign.center),
+        Container(child: OutlineButton(
+          borderSide: new BorderSide(color: AppConst.themeMainColor),
+          child: new Text('复制地址', style: TextStyle(color: AppConst.themeMainColor)),
+          onPressed: () {
+            
+          },
+        ),),
+      ],),
+    );
+  }
+
+
+  Widget _qrcodeWidget(String rechargeAddress) {
+    return QrImage(
+      data: rechargeAddress,
+      version: QrVersions.auto,
+      size: 180,
+      gapless: false,
+      errorStateBuilder: (context, err) {
+        return Container(
+          child: Text(
+            "Uh oh! Something went wrong...",
+            textAlign: TextAlign.center,
+          ),
+        );
+      },
+    );
+  }
 }
